75a2914e08b07c54712dfa37a9e559be49b82f2f
yanz17 <dwiantoyan@gmail.com>
2024-06-11 13:32:32 +0700

    add test for like and unlike feature

---
+++ tests/unlikeRestaurant.test.js
@@ -0,0 +1,66 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
+
+describe('Unliking A Restaurant', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+
+  it('should display unlike widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this movie"]')).toBeTruthy();
+  });
+
+  it('should not display like widget when the restaurant has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this movie"]')).toBeFalsy();
+  });
+
+  it('should be able to remove liked restaurant from the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+    expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([]);
+  });
+
+  it('should not throw error when user click unlike widget if the unliked restaurant is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    await FavoriteRestaurantIdb.deleteRestaurant(1);
+
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+    expect(await FavoriteRestaurantIdb.getAllRestaurant()).toEqual([]);
+  });
+});
